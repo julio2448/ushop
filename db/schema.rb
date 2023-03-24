dd645747153492c1092c16aee6f4b818fce4f4f3
Julio Vazquez <jv4271@gmail.com>
2023-03-24 11:10:52 -0600

    geo2

--- db/schema.rb
+++ db/schema.rb
@@ -73,15 +73,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_222206) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
+    t.string "role"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
-=======
-    t.string "role"
-    t.string "address"
     t.boolean "admin"
->>>>>>> master
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
